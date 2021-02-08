ac086c310e1ca3fdc96ca83d345d88be31ab96e4
Alexandre Laranjeiras <laranja22@hotmail.com>
2021-02-07 22:00:04 -0300

    Added MergeContract

---
+++ Zion.Common/Extensions/ContractExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zion.Common.NotificationPattern
+{
+    public static class ContractExtension
+    {
+        public static Contract MergeContract(this Contract contractDestination, Contract contractSource)
+        {
+            if (contractSource == null)
+                throw new ArgumentNullException("MergeContract received null values");
+
+            contractDestination.AddNotifications(contractSource.Notifications);
+
+            return contractDestination;
+        }
+    }
+}
